95a4d7001b1e7aad493f5add53fbd71940fee675
Per Johansson <mail@perjohansson.net>
2024-04-21 01:04:17 +0200

    trampoline stuff

--- src/cv_pixel_buffer.rs
+++ src/cv_pixel_buffer.rs
@@ -6,7 +6,8 @@ mod internal {
     use core_foundation::base::{kCFAllocatorDefault, Boolean, CFAllocatorRef, CFTypeID, TCFType};
     use core_foundation::dictionary::CFDictionaryRef;
     use core_foundation::{declare_TCFType, impl_TCFType};
-    use core_utils_rs::ref_con::{trampoline_reversed, TrampolineRefCon};
+    use core_utils_rs::declare_trampoline;
+    use core_utils_rs::ref_con::TrampolineRefCon;
     use four_char_code::FourCharCode;
     use io_surface::IOSurfaceRef;
     use std::ffi::c_void;
@@ -21,7 +22,15 @@ mod internal {
     impl_TCFType!(CVPixelBuffer, CVPixelBufferRef, CVPixelBufferGetTypeID);
 
     type CVPixelBufferReleaseBytesCallback =
-        unsafe extern "C" fn(releaseRefCon: *mut TrampolineRefCon, baseAddress: *mut c_void);
+        unsafe extern "C" fn(releaseRefCon: *mut TrampolineRefCon, planebaseaddresses: *mut [u8]);
+
+    type CVPixelBufferReleaseBytesPlanarCallback = unsafe extern "C" fn(
+        releaseRefCon: *mut TrampolineRefCon,
+        dataPtr: *mut c_void,
+        dataSize: usize,
+        numberOfPlanes: usize,
+        planebaseaddresses: *mut [u8],
+    );
 
     #[link(name = "CoreVideo", kind = "framework")]
     extern "C" {
@@ -41,13 +50,13 @@ mod internal {
             allocator: CFAllocatorRef,
             width: usize,
             height: usize,
-            pixelFormatType: OSType,
-            baseAddress: *mut c_void,
-            bytesPerRow: usize,
-            releaseCallback: CVPixelBufferReleaseBytesCallback,
-            releaseRefCon: *mut TrampolineRefCon,
-            pixelBufferAttributes: CFDictionaryRef,
-            pixelBufferOut: *mut CVPixelBufferRef,
+            pixel_format_type: OSType,
+            base_address: *mut c_void,
+            bytes_per_row: usize,
+            release_callback: CVPixelBufferReleaseBytesCallback,
+            release_ref_con: *mut TrampolineRefCon,
+            pixel_buffer_attributes: CFDictionaryRef,
+            pixel_buffer_out: *mut CVPixelBufferRef,
         ) -> CVReturn;
 
         fn CVPixelBufferCreateWithPlanarBytes(
@@ -62,7 +71,7 @@ mod internal {
             planeWidth: *const usize,
             planeHeight: *const usize,
             planeBytesPerRow: *const usize,
-            releaseCallback: CVPixelBufferReleaseBytesCallback,
+            releaseCallback: CVPixelBufferReleaseBytesPlanarCallback,
             releaseRefCon: *mut TrampolineRefCon,
             pixelBufferAttributes: CFDictionaryRef,
             pixelBufferOut: *mut CVPixelBufferRef,
@@ -161,22 +170,35 @@ mod internal {
     ) -> Result<CVPixelBuffer, CVPixelBufferError>
     where
         TRefCon: Sized,
-        TReleaseCallback: FnMut(TRefCon, *mut c_void) -> Result<(), CVPixelBufferError>,
+        TReleaseCallback: FnMut(TRefCon, &mut [u8]),
         TReleaseCallback: Send + 'static,
     {
         let mut pixel_buffer_out: CVPixelBufferRef = ptr::null_mut();
         if base_address.len() < bytes_per_row * height {
             return Err(CVPixelBufferError::InvalidSize);
         }
+        pub unsafe extern "C" fn release_callback_trampoline<
+            TRefcon,
+            FCallback: FnMut(TRefcon, &mut [u8]) + Send + 'static,
+        >(
+            refcon: *mut TrampolineRefCon,
+            planebaseaddresses: *mut [u8],
+        ) {
+            let refcon_data = &*(refcon);
+            let mut user_data: FCallback = ptr::read(refcon_data.0.cast());
+            user_data(ptr::read(refcon_data.0.cast()), &mut *planebaseaddresses);
+            ptr::drop_in_place(refcon);
+        };
+        println!("first: {:p}", base_address);
         unsafe {
             let result = CVPixelBufferCreateWithBytes(
                 kCFAllocatorDefault,
                 width,
                 height,
                 pixel_format_type.as_u32(),
-                base_address.as_mut_ptr().cast(),
+                base_address as *mut _ as *mut c_void,
                 bytes_per_row,
-                trampoline_reversed::<*mut c_void, CVPixelBufferError, TRefCon, TReleaseCallback>,
+                release_callback_trampoline::<TRefCon, TReleaseCallback>,
                 TrampolineRefCon::new(Some(release_ref_con), release_callback)
                     .into_leaked_mut_ptr(),
                 pixel_buffer_attributes,
@@ -190,11 +212,11 @@ mod internal {
         }
     }
 
-    pub fn create_with_bytes_planar<TRefCon, TReleaseCallback>(
+    pub fn create_with_bytes_planar<TReleaseRefcon, TReleaseCallback, TDataPtr>(
         width: usize,
         height: usize,
         pixel_format_type: FourCharCode,
-        data_ptr: Option<&mut [u8]>,
+        data_ptr: Option<&mut TDataPtr>,
         data_size: Option<usize>,
         number_of_planes: usize,
         plane_base_addresses: &[&mut [u8]],
@@ -202,12 +224,18 @@ mod internal {
         plane_height: &[usize],
         plane_bytes_per_row: &[usize],
         release_callback: TReleaseCallback,
-        release_ref_con: TRefCon,
+        release_ref_con: TReleaseRefcon,
         pixel_buffer_attributes: CFDictionaryRef,
     ) -> Result<CVPixelBuffer, CVPixelBufferError>
     where
-        TRefCon: Sized,
-        TReleaseCallback: FnMut(TRefCon, *mut c_void) -> Result<(), CVPixelBufferError>,
+        TReleaseRefcon: Sized,
+        TReleaseCallback: FnMut(
+            TReleaseRefcon,
+            Option<TDataPtr>,
+            Option<usize>,
+            usize,
+            &mut [&mut [u8]],
+        ) -> Result<(), CVPixelBufferError>,
         TReleaseCallback: Send + 'static,
     {
         let mut pixel_buffer_out: CVPixelBufferRef = ptr::null_mut();
@@ -219,31 +247,32 @@ mod internal {
         {
             return Err(CVPixelBufferError::InvalidSize);
         }
-        unsafe {
-            let result = CVPixelBufferCreateWithPlanarBytes(
-                kCFAllocatorDefault,
-                width,
-                height,
-                pixel_format_type.as_u32(),
-                data_ptr.map_or(ptr::null_mut(), |v| v.as_mut_ptr().cast()),
-                data_size.map_or(0, |v| v),
-                number_of_planes,
-                plane_base_addresses.as_ptr().cast(),
-                plane_width.as_ptr(),
-                plane_height.as_ptr(),
-                plane_bytes_per_row.as_ptr(),
-                trampoline_reversed::<*mut c_void, CVPixelBufferError, TRefCon, TReleaseCallback>,
-                TrampolineRefCon::new(Some(release_ref_con), release_callback)
-                    .into_leaked_mut_ptr(),
-                pixel_buffer_attributes,
-                &mut pixel_buffer_out,
-            );
-            if result == CV_RETURN_SUCCESS {
-                Ok(CVPixelBuffer::wrap_under_create_rule(pixel_buffer_out))
-            } else {
-                Err(CVPixelBufferError::from(result))
-            }
-        }
+        todo!();
+        // unsafe {
+        //     let result = CVPixelBufferCreateWithPlanarBytes(
+        //         kCFAllocatorDefault,
+        //         width,
+        //         height,
+        //         pixel_format_type.as_u32(),
+        //         data_ptr.map_or(ptr::null_mut(), |v| v as *mut TDataPtr as *mut c_void),
+        //         data_size.map_or(0, |v| v),
+        //         number_of_planes,
+        //         plane_base_addresses.as_ptr().cast(),
+        //         plane_width.as_ptr(),
+        //         plane_height.as_ptr(),
+        //         plane_bytes_per_row.as_ptr(),
+        //         release_callback_trampoline::<TReleaseCallback>,
+        //         TrampolineRefCon::new(Some(release_ref_con), release_callback)
+        //             .into_leaked_mut_ptr(),
+        //         pixel_buffer_attributes,
+        //         &mut pixel_buffer_out,
+        //     );
+        //     if result == CV_RETURN_SUCCESS {
+        //         Ok(CVPixelBuffer::wrap_under_create_rule(pixel_buffer_out))
+        //     } else {
+        //         Err(CVPixelBufferError::from(result))
+        //     }
+        // }
     }
 
     #[cfg(test)]
@@ -276,16 +305,18 @@ mod internal {
         fn test_create_with_bytes() -> Result<(), crate::cv_pixel_buffer_error::CVPixelBufferError>
         {
             use super::*;
-            let buffer = &mut vec![0];
-            buffer.resize(1920 * 1080 * 2, 0);
+            let mut buffer = &mut vec![];
+            buffer.resize(1920 * 1080 * 2, 4);
+            let b = buffer.as_mut_slice();
+            println!("second: {:p}", b);
             let pixel_buffer = create_with_bytes(
                 1920,
                 1080,
                 FourCharCode::from_str("2vuy").unwrap(),
-                buffer.as_mut(),
+                b,
                 1920 * 2,
-                |_: (), _| Ok(()),
-                (),
+                |refcon, address| println!("Release {refcon} {address:?}"),
+                32,
                 ptr::null(),
             )?;
             assert_eq!(
@@ -303,41 +334,41 @@ mod internal {
             Ok(())
         }
 
-        #[test]
-        fn test_create_with_bytes_planar(
-        ) -> Result<(), crate::cv_pixel_buffer_error::CVPixelBufferError> {
-            use super::*;
-            let buffer = &mut vec![0];
-            buffer.resize(1920 * 1080 * 2, 0);
-            let pixel_buffer = create_with_bytes_planar(
-                1920,
-                1080,
-                FourCharCode::from_str("2vuy").unwrap(),
-                None,
-                None,
-                1,
-                &[buffer],
-                &[1920],
-                &[1080],
-                &[1920 * 2],
-                |_: (), _| Ok(()),
-                (),
-                ptr::null(),
-            )?;
-            assert_eq!(
-                unsafe { CVPixelBufferGetWidth(pixel_buffer.as_concrete_TypeRef()) },
-                1920
-            );
-            assert_eq!(
-                unsafe { CVPixelBufferGetHeight(pixel_buffer.as_concrete_TypeRef()) },
-                1080
-            );
-            assert_eq!(
-                unsafe { CVPixelBufferGetPixelFormatType(pixel_buffer.as_concrete_TypeRef()) },
-                FourCharCode::from_str("2vuy").unwrap().as_u32()
-            );
-            Ok(())
-        }
+        // #[test]
+        // fn test_create_with_bytes_planar(
+        // ) -> Result<(), crate::cv_pixel_buffer_error::CVPixelBufferError> {
+        //     use super::*;
+        //     let buffer = &mut vec![0];
+        //     buffer.resize(1920 * 1080 * 2, 0);
+        //     let pixel_buffer = create_with_bytes_planar(
+        //         1920,
+        //         1080,
+        //         FourCharCode::from_str("2vuy").unwrap(),
+        //         None,
+        //         None,
+        //         1,
+        //         &[buffer],
+        //         &[1920],
+        //         &[1080],
+        //         &[1920 * 2],
+        //         |_: (), _: (), _, _, _| Ok(()),
+        //         (),
+        //         ptr::null(),
+        //     )?;
+        //     assert_eq!(
+        //         unsafe { CVPixelBufferGetWidth(pixel_buffer.as_concrete_TypeRef()) },
+        //         1920
+        //     );
+        //     assert_eq!(
+        //         unsafe { CVPixelBufferGetHeight(pixel_buffer.as_concrete_TypeRef()) },
+        //         1080
+        //     );
+        //     assert_eq!(
+        //         unsafe { CVPixelBufferGetPixelFormatType(pixel_buffer.as_concrete_TypeRef()) },
+        //         FourCharCode::from_str("2vuy").unwrap().as_u32()
+        //     );
+        //     Ok(())
+        // }
     }
 }
 pub use internal::{CVPixelBuffer, CVPixelBufferRef};
